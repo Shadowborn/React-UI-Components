03907c67ae05a56ddea10169b761b72632955b75
Shadowborn <james.starks023@gmail.com>
2019-04-22 16:06:14 -0600

    Created header and card components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,23 @@
 import React from 'react';
+
+import HeaderTitle from './HeaderTitle'
+import HeaderSubtitle from './HeaderSubtitle'
+import ImageThumbnail from './ImageThumbnail'
+
 import './Header.css';
+
+const HeaderContent = () => {
+    return (
+    <div className="header-content">
+    <ImageThumbnail />
+    <HeaderTitle />
+    <HeaderSubtitle />
+    <p className="headerP">Let's Learn react by building simple interfaces with
+    components. Don't try to overthink it, just keep it simple and have fun.
+    Once you feel comfortable using components you are well on your way to
+    mastering react.</p>
+    </div>
+    );
+};
+
+export default HeaderContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+
+
+const HeaderTitle = () => {
+    return <div className="header-title">
+    <h3 className="title">Lambda School</h3>
+    </div>
+};
+
+export default HeaderTitle;
