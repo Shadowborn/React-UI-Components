51af0688f1c4c4fc873577c5ffb29e540ff2f8c7
Shadowborn <james.starks023@gmail.com>
2019-04-22 16:34:53 -0600

    card link added

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -7,10 +7,11 @@ import CardBanner from './CardBanner'
 const CardContainer = () => {
     return (
     <div className="card-container">
+    <a href="https://www.reactjs.org">
     <CardBanner />
     <CardContent />
     
-    
+    </a>
     </div>
     );
 };
